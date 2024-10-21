e4f2f32e3781b7be37a30673a936d405b4429357
범 <kbs9780@gmail.com>
2024-10-21 19:57:14 +0900

    [feat] 백오피스 가입,로그인 기능추가

--- src/com/happySteps/backoffice/users/controller/ManagerWeb.java
+++ src/com/happySteps/backoffice/users/controller/ManagerWeb.java
@@ -1,13 +1,13 @@
 /**
  * YOU ARE STRICTLY PROHIBITED TO COPY, DISCLOSE, DISTRIBUTE, MODIFY OR USE THIS PROGRAM
- * IN PART OR AS A WHOLE WITHOUT THE PRIOR WRITTEN CONSENT OF HIMEDIA.CO.KR.
- * HIMEDIA.CO.KR OWNS THE INTELLECTUAL PROPERTY RIGHTS IN AND TO THIS PROGRAM.
- * COPYRIGHT (C) 2024 HIMEDIA.CO.KR ALL RIGHTS RESERVED.
+ * IN PART OR AS A WHOLE WITHOUT THE PRIOR WRITTEN CONSENT OF happySteps.com.
+ * happySteps.com OWNS THE INTELLECTUAL PROPERTY RIGHTS IN AND TO THIS PROGRAM.
+ * COPYRIGHT (C) 2024 happySteps.com ALL RIGHTS RESERVED.
  *
- * 하기 프로그램에 대한 저작권을 포함한 지적재산권은 himedia.co.kr에 있으며,
- * himedia.co.kr이 명시적으로 허용하지 않는 사용, 복사, 변경 및 제 3자에 의한 공개, 배포는 엄격히 금지되며
- * himedia.co.kr의 지적재산권 침해에 해당된다.
- * Copyright (C) 2024 himedia.co.kr All Rights Reserved.
+ * 하기 프로그램에 대한 저작권을 포함한 지적재산권은 happySteps.com에 있으며,
+ * happySteps.com이 명시적으로 허용하지 않는 사용, 복사, 변경 및 제 3자에 의한 공개, 배포는 엄격히 금지되며
+ * happySteps.com의 지적재산권 침해에 해당된다.
+ * Copyright (C) 2024 happySteps.com All Rights Reserved.
  *
  *
  * Program		: com.happySteps
@@ -16,9 +16,11 @@
  * File			: ManagerWeb.java
  * Notes		:
  * History		: [NO][Programmer][Description]
- *				: [20240726154947][pluto@himedia.co.kr][CREATE: Initial Release]
+ *				: [20240726154947][kbs@happySteps.com][CREATE: Initial Release]
  */
 package com.happySteps.backoffice.users.controller;
+
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
@@ -27,16 +29,20 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.happySteps.backoffice.common.Common;
+import com.happySteps.backoffice.common.dto.PagingDto;
+import com.happySteps.backoffice.common.dto.PagingListDto;
 import com.happySteps.backoffice.users.dto.ManagerDto;
 import com.happySteps.backoffice.users.service.ManagerSrvc;
 /**
  * @version 1.0.0
- * @author pluto@himedia.co.kr
+ * @author kbs@happySteps.com
  * 
  * @since 2024-07-26
  * <p>DESCRIPTION:</p>
@@ -50,6 +56,97 @@ public class ManagerWeb extends Common {
 	
 	@Inject
 	private ManagerSrvc managerSrvc;
+	
+	@RequestMapping(value = "/console/users/manager/checkIdDuplicate.json", method = RequestMethod.POST, 
+				headers = {"content-type=application/json; charset=UTF-8", "accept=application/json"}, 
+				consumes="application/json; charset=UTF-8", produces="application/json; charset=UTF-8")
+	public @ResponseBody boolean checkIdDuplicate(@RequestBody ManagerDto managerDto) {
+	
+	boolean idDuplicate = true;
+	
+	try {
+		managerDto.setId(managerDto.getId());
+		
+		int count = managerSrvc.selectIdDuplicate(managerDto);
+		
+		if (count == 0) idDuplicate = false;
+		
+	}
+	catch (Exception e) {
+		logger.error("[" + this.getClass().getName() + ".checkIdDuplicate()] " + e.getMessage(), e);
+	}
+	finally {}
+	
+	return idDuplicate;
+	}
+	@RequestMapping(value = "/console/users/manager/checkNickDuplicate.json", method = RequestMethod.POST, 
+				headers = {"content-type=application/json; charset=UTF-8", "accept=application/json"}, 
+				consumes="application/json; charset=UTF-8", produces="application/json; charset=UTF-8")
+	public @ResponseBody boolean checkNickDuplicate(@RequestBody ManagerDto managerDto) {
+	
+	boolean nickDuplicate = true;
+	
+	try {
+		
+		managerDto.setNickname(managerDto.getNickname());
+		
+		int count = managerSrvc.selectNickDuplicate(managerDto);
+		
+		if (count == 0) nickDuplicate = false;
+		
+	}
+	catch (Exception e) {
+		logger.error("[" + this.getClass().getName() + ".checkNickDuplicate()] " + e.getMessage(), e);
+	}
+	finally {}
+	
+	return nickDuplicate;
+	}
+	
+	/**
+	 * @param request [요청 서블릿]
+	 * @param response [응답 서블릿]
+	 * @param managerDto [관리자 빈]
+	 * @return ModelAndView
+	 * 
+	 * @since 2024-07-26
+	 * <p>DESCRIPTION: 가입 처리</p>
+	 * <p>IMPORTANT:</p>
+	 * <p>EXAMPLE:</p>
+	 */
+	@RequestMapping(value = "/console/users/manager/list.web")
+	public ModelAndView list(HttpServletRequest request, HttpServletResponse response, PagingDto pagingDto) {
+		
+		ModelAndView mav = new ModelAndView("redirect:/error.web");
+		
+		try {
+			
+			String searchWord = pagingDto.getSearchWord();
+			pagingDto.setSearchWord(searchWord);
+			
+			PagingListDto pagingListDto = managerSrvc.list(pagingDto);
+			
+			@SuppressWarnings("unchecked")
+			List<ManagerDto> list = (List<ManagerDto>) pagingListDto.getList();
+			
+			for (int loop = 0; loop < list.size(); loop++) {
+				list.get(loop).setId(list.get(loop).getId());
+				list.get(loop).setMng_nm(list.get(loop).getMng_nm());
+			}
+			
+			pagingDto.setSearchWord(searchWord);
+			mav.addObject("paging"	, pagingListDto.getPaging());
+			mav.addObject("list"	, pagingListDto.getList());
+			
+			mav.setViewName("backoffice/users/manager/list");
+		}
+		catch (Exception e) {
+			logger.error("[" + this.getClass().getName() + ".list()] " + e.getMessage(), e);
+		}
+		finally {}
+		
+		return mav;
+	}
 	
 	/**
 	 * @param request [요청 서블릿]
--- src/com/happySteps/backoffice/users/dto/ManagerDto.java
+++ src/com/happySteps/backoffice/users/dto/ManagerDto.java
@@ -1,35 +1,36 @@
 /**
  * YOU ARE STRICTLY PROHIBITED TO COPY, DISCLOSE, DISTRIBUTE, MODIFY OR USE THIS PROGRAM
- * IN PART OR AS A WHOLE WITHOUT THE PRIOR WRITTEN CONSENT OF HIMEDIA.CO.KR.
- * HIMEDIA.CO.KR OWNS THE INTELLECTUAL PROPERTY RIGHTS IN AND TO THIS PROGRAM.
- * COPYRIGHT (C) 2024 HIMEDIA.CO.KR ALL RIGHTS RESERVED.
+ * IN PART OR AS A WHOLE WITHOUT THE PRIOR WRITTEN CONSENT OF happySteps.com.
+ * happySteps.com OWNS THE INTELLECTUAL PROPERTY RIGHTS IN AND TO THIS PROGRAM.
+ * COPYRIGHT (C) 2024 happySteps.com ALL RIGHTS RESERVED.
  *
- * 하기 프로그램에 대한 저작권을 포함한 지적재산권은 himedia.co.kr에 있으며,
- * himedia.co.kr이 명시적으로 허용하지 않는 사용, 복사, 변경 및 제 3자에 의한 공개, 배포는 엄격히 금지되며
- * himedia.co.kr의 지적재산권 침해에 해당된다.
- * Copyright (C) 2024 himedia.co.kr All Rights Reserved.
+ * 하기 프로그램에 대한 저작권을 포함한 지적재산권은 happySteps.com에 있으며,
+ * happySteps.com이 명시적으로 허용하지 않는 사용, 복사, 변경 및 제 3자에 의한 공개, 배포는 엄격히 금지되며
+ * happySteps.com의 지적재산권 침해에 해당된다.
+ * Copyright (C) 2024 happySteps.com All Rights Reserved.
  *
  *
- * Program		: kr.co.himedia.ecommerce
+ * Program		: com.happySteps
  * Description	:
  * Environment	: JRE 1.7 or more
  * File			: ManagerDto.java
  * Notes		:
  * History		: [NO][Programmer][Description]
- *				: [20240726171131][pluto@himedia.co.kr][CREATE: Initial Release]
+ *				: [20240726171131][kbs@happySteps.com][CREATE: Initial Release]
  */
 package com.happySteps.backoffice.users.dto;
 
 /**
  * @version 1.0.0
- * @author pluto@himedia.co.kr
+ * @author kbs@happySteps.com
  * 
- * @since 2024-07-26
+ * @since 2024-10-21
  * <p>DESCRIPTION:</p>
  * <p>IMPORTANT:</p>
  */
 public class ManagerDto {
 	
+	private int rnum			= 0;
 	private int seq_mng		= 0;
 	private String id			= "";
 	private String nickname		= "";
@@ -46,6 +47,13 @@ public class ManagerDto {
 	private String dt_upt		= "";
 	private int updater		= 0;
 	
+	
+	public int getRnum() {
+		return rnum;
+	}
+	public void setRnum(int rnum) {
+		this.rnum = rnum;
+	}
 	public String getNickname() {
 		return nickname;
 	}
